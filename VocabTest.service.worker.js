f51f0b233983d74af581f1a48e09b46d72a9be8b
JOJO <39382270+GCJOJO@users.noreply.github.com>
2026-02-10 15:25:28 +0100

    v0.0.2 - Basic Redesign

--- VocabTest.service.worker.js
+++ VocabTest.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770576009|2138204091';
+const CACHE_VERSION = '1770733499|7165574744';
 /** @type {string} */
 const CACHE_PREFIX = 'VocabTest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
